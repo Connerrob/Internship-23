f9e278c4a4273cb45cfde0a2bfca363de20b8d87
Connerrob <122704959+Connerrob@users.noreply.github.com>
2023-07-27 09:58:35 -0400

    Update theme.js

--- theme.js
+++ theme.js
@@ -6,7 +6,10 @@ function toggleTheme() {
     const main = document.querySelector('main');
     const learned = document.querySelector('.learned');
     const footer = document.querySelector('footer');
-
+    const learnedItems = document.querySelectorAll('.learned-items');
+    const project = document.querySelector('.projects-container');
+    const gitbutton = document.querySelector('.git-button');
+    const topbutton = document.querySelector('.up-button');
 
     if (header.classList.contains('light-mode')) {
       header.classList.remove('light-mode');
@@ -21,8 +24,18 @@ function toggleTheme() {
       main.classList.add('dark-mode');
       learned.classList.remove('light-mode');
       learned.classList.add('dark-mode');
+      learnedItems.forEach(learnedItem => {
+        learnedItem.classList.remove('light-mode');
+        learnedItem.classList.add('dark-mode');
+      });
       footer.classList.remove('light-mode');
       footer.classList.add('dark-mode');
+      project.classList.remove('light-mode');
+      project.classList.add('dark-mode');
+      gitbutton.classList.remove('light-mode');
+      gitbutton.classList.add('dark-mode');
+      topbutton.classList.remove('light-mode');
+      topbutton.classList.add('dark-mode');
     } else {
       header.classList.remove('dark-mode');
       header.classList.add('light-mode');
@@ -36,9 +49,18 @@ function toggleTheme() {
       main.classList.add('light-mode');
       learned.classList.remove('dark-mode');
       learned.classList.add('light-mode');
+      learnedItems.forEach(learnedItem => {
+        learnedItem.classList.remove('dark-mode');
+        learnedItem.classList.add('light-mode');
+      });
       footer.classList.remove('dark-mode');
       footer.classList.add('light-mode');
+      project.classList.remove('dark-mode');
+      project.classList.add('light-mode');
+      gitbutton.classList.remove('dark-mode');
+      gitbutton.classList.add('light-mode');
+      topbutton.classList.remove('dark-mode');
+      topbutton.classList.add('light-mode');
     }
    }
-   
    
